8923c22c3499d0b63f0ed7e72b6f53bf5e7b622e
Tran Cong Toan <22110248@student.hcmute.edu.vn>
2023-11-20 20:42:03 +0700

    added a* algo

--- backend/app/map.py
+++ backend/app/map.py
@@ -5,6 +5,7 @@ import plotly.express as px
 import heapq 
 from math import *
 
+DEFAULT_GOAL_RADIUS = 20
 FAILURE = 'FAILURE'
 def HaversineDistance(first, second):
     lat1, lon1 = first
@@ -19,21 +20,17 @@ def HaversineDistance(first, second):
     return R * c
 
 
-
-class Node():
-    def __init__(self, parent=None, position=None):
+class MapNode:
+    def __init__(self, parent=None, id=None):
         self.parent = parent
-        self.position = position    
+        self.id = id     
 
         self.g = 0
         self.h = 0
         self.f = 0
 
     def __eq__(self, other):
-        return self.position == other.position
-
-# class AStar(map, start, end):
-
+        return self.info == other.info
 
 
 class Map: 
@@ -77,12 +74,14 @@ class Map:
             self.graph = json.load(f)    
             for node in self.graph.keys():
                 coordinate = self.graph[node]['info']['geometry']['coordinates']
-                self.coordinates.append([ coordinate[0], coordinate[1]])
+                self.coordinates.append([ coordinate[1], coordinate[0]])
                 self.nodes.append(node)
             self.tree = KDTree(self.coordinates)
         with open('./app/building.json', 'r') as f:
             self.buildings = json.load(f)
             self.getAllBuildings()
+
+        # self.showMap(list(self.graph.keys()), self.coordinates)
 
     def getAllBuildings(self):
         all_buildings = []
@@ -103,58 +102,70 @@ class Map:
     def getDestionaion(self):
         return self.graph[self.nodes[0]]
 
-    class Node:
-        def __init__(self, parent=None, id=None):
-            self.parent = parent
-            self.id = id     
-
-            self.g = 0
-            self.h = 0
-            self.f = 0
-
-        def __eq__(self, other):
-            return self.id == other.id
-    
+ 
     def getNodeCoordinateById(self, id):
         node = self.graph[id]["info"]["geometry"]["coordinates"]
         return node
 
+    def getDistanceBetweenId(self, x, y):
+        return HaversineDistance(self.getNodeCoordinateById(x), self.getNodeCoordinateById(y))
+
     def getNeighbors(self, id):
-        data = self.graph[id]['neighbors']
-        neighbors = []
-        for neighbor in data:
-            neighbors.append((neighbor, HaversineDistance(self.getNodeCoordinateById(id), self.getNodeCoordinateById(neighbor))))
-        return neighbors
+        return self.graph[id]['neighbors']
+        # neighbors = []
+        # for neighbor in data:
+        #     # print(neighbor)
 
-    def AStar(self, start, goal_test, graph):
-        # openSet = [new Node(start)]
-        # node = Node(None, start)
-        # frontier = [node]
-        # explored = []
+        #     neighbor
+        #     # neighbors.append((neighbor, self.getDistanceBetweenId(id, neighbor)))
+        # return neighbors
 
-        # while(len(frontier) > 0):
-        #     node = heapq.heappop(frontier)
-        #     if(goal_test(node.id)):
-        #         return node
-        #     explored.append(node)
-        #     for action in self.getNeighbors(node.id):   
-        #         child = Node(node, action)
-        #         if(not ((child in frontier) or (child in explored))):
-        #             heapq.heappush(frontier, child)
-        #         elif:
-                    
+    def AStar(self, start, goal_test):
+        node = MapNode(None, start)
+        open_list = [node]
+        closed_list = []
+
+        while(len(open_list) > 0):
+            node = heapq.heappop(open_list)
+            node.f = node.g + node.h
+            if (goal_test(node.id)):
+                return ['FOUND SOLUTION!']
+            children = self.getNeighbors(node.id)
+
+            successors = []
+            for child in children:
+                successors.append(MapNode(parent=node, id=child))
+
+            for successor in successors:
+                # f = g + heuristic
+                child_current_cost = node.g + self.getDistanceBetweenId(node.id, successor.id)
+
         return FAILURE
 
+    def getBuildingCoordinates(self, id):
+        return self.buildings[id]["geometry"]["coordinates"][0]
+    
 
     def findShortestPath(self, lat, lon, type, target_id):
         nodes = ['root']
         coords = [[lat, lon]]
+        def goalTestByBuildingId(current_node_id, target_id=target_id):
+            node_pos = self.getNodeCoordinateById(current_node_id)
+            coordinates = self.getBuildingCoordinates(target_id)
+            min_distance = HaversineDistance(node_pos, coordinates[0])
+            for coordinate in coordinates:
+                print(node_pos, coordinate, HaversineDistance(node_pos, coordinate))
+                min_distance = min(min_distance, HaversineDistance(node_pos, coordinate))
+            return min_distance <= DEFAULT_GOAL_RADIUS
 
-        start_node = self.getNearestNode(lon, lat)
-        neighbors = self.getNeighbors(start_node)
-        for neighbor in neighbors:
-            nodes.append(neighbor[0])
-            coords.append(list(reversed(self.getNodeCoordinateById(neighbor[0]))))
-
+        start_node = self.getNearestNode(lat, lon)
+        # neighbors = self.getNeighbors(start_node)
+        self.AStar(start_node, goalTestByBuildingId)
+        # print(self.goalTestByBuildingId(start_node, target_id))
+        # for neighbor in neighbors:
+        #     nodes.append(neighbor[0])
+        #     print(start_node, '->',neighbor[0])
+        #     coords.append(list(reversed(self.getNodeCoordinateById(neighbor[0]))))
+        # print(neighbors)
 
         # self.showMap(nodes, coords)
